177dcb2d318c84a4c07bebd323ebc62a8a4dbf27
Leonidas Brockweld <leonidasleo10@gmail.com>
2025-04-26 11:25:26 -0300

    Ajustado backend para permitir requisiçoes de multiplos endereços e ajustado front apra fazer requisiçoes simples, mostradas no console do navegador

---
+++ backend/delivery/src/main/java/com/brothers/delivery/security/filter/SimpleCORSFilter.java
@@ -0,0 +1,30 @@
+package com.brothers.delivery.security.filter;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SimpleCORSFilter {
+    /**
+     * Configura o CORS para permitir requisições de origens específicas.
+     * 
+     * @return WebMvcConfigurer com as configurações de CORS.
+     */
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                    .allowedOrigins("http://127.0.0.1:5501")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                    .allowedHeaders("*")
+                    .allowCredentials(true);
+            }
+        };
+    }
+}
+
