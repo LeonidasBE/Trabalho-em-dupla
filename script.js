8a9f2b7d0857de46e03787e375ed361b750eadf3
Matheus-Goebel <Matheus_lol_@hotmail.com>
2025-05-06 16:56:27 -0300

    added animations to the page and modified a function (wip)

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const header = document.querySelector("header");
 const urlPadrao = "http://localhost:8080/api/pedido/pedidos";
+const gameImages = document.querySelector('.gameImages');
+gameImages.innerHTML += gameImages.innerHTML; 
+gameImages.innerHTML += gameImages.innerHTML; 
 
 window.addEventListener("scroll",function(){
     header.classList.toggle("sticky",window.scrollY > 10);
@@ -11,17 +14,42 @@ function listarPedidos() {
     })
 }
 
-function realizarPedido() {
-    const novoPedido = {
-        cliente : "teste",
-        enderecoEntrega : "endereço Teste",
-        status : "Pendente",
-        valorTotal : 10.0,
-    }
+// function realizarPedido() {
+//     const novoPedido = {
+//         cliente : "teste",
+//         enderecoEntrega : "endereço Teste",
+//         status : "Pendente",
+//         valorTotal : 10.0,
+//     }
 
-    httpPostListPedidosAsync(novoPedido, function(response){
+//     httpPostListPedidosAsync(novoPedido, function(response){
+//         console.log('Pedido realizado', response);
+//     })
+// }
+
+
+function realizarPedido(elemento) {
+    const row = elemento.closest(".row");
+    const imagem = row.querySelector("img").getAttribute("src");
+    const nome = row.querySelector("h3").textContent;
+    const preco = row.querySelector(".price h6").textContent;
+    const descricao = row.querySelector("p").textContent;
+
+    const novoPedido = {
+        cliente: "teste",
+        enderecoEntrega: "endereço Teste",
+        status: "Pendente",
+        nomeJogo: nome,
+        imagem: imagem,
+        valorTotal: preco,
+        descricao: descricao,
+    };
+
+    console.log('Pedido realizado', novoPedido);
+
+    httpPostListPedidosAsync(novoPedido, function(response) {
         console.log('Pedido realizado', response);
-    })
+    });
 }
 
 function atualizarPedido() {
